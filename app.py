bcfe7eb9265a3505760396ae7b0ff92996668e19
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-03-04 01:32:19 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+from flask import Flask, request,render_template
+app = Flask(__name__)
+#@app.route("/", methods=['GET', 'POST'])
+#def index():
+#    return "hello man"
+@app.route("/", methods=['GET', 'POST'])
+def menu():
+         MONGODB_URI="mongodb://Debangshu:Starrynight.1@ds163694.mlab.com:63694/brilu"
+         client = MongoClient(MONGODB_URI, connectTimeoutMS=30000)
+         db = client.get_database("brilu")
+         col = db["restaurants"]
+         cursor = col.find()
+         restaurant = cursor[0]
+         print(restaurant[restaurant]["menu"])
+         return render_template("headerfooter.html",menu=restaurant[restaurant]["menu"])
+         #return render_template('chart.html',menu=menu)
