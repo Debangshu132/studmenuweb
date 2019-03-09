133c6251fe173d63c1a82a01977b88b40be65885
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-03-09 18:50:50 +0530

    Update main.js

--- static/main.js
+++ static/main.js
@@ -243,7 +243,7 @@ for (fooddrink in menuitemjson) {
     var fooditem;
     for(fooditem in menuitemjson[fooddrink][category]){
       var stringfooditem=JSON.stringify(fooditem);
-      if( stringfooditem.includes(textvalue)){
+      if( stringfooditem.includes(textvalue.toLowerCase())){
 
          var price=menuitemjson[fooddrink][category][fooditem];
          jsonofitems[stringfooditem]=price;
