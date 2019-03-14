e9399414afa210d4f978327afa1db1c1970133e7
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-03-14 15:22:29 +0530

    Update main.js

--- static/main.js
+++ static/main.js
@@ -166,12 +166,13 @@ document.getElementById("carticon").onclick=function(menuPicArray){
   cartorderbutton.onclick=function(){
     var http = new XMLHttpRequest();
     //http.setRequestHeader("Content-Type", "application/json");
-    var url = 'http://www.studmenu.herokuapp.com/cart/'+JSON.stringify(window.cart);
-    var params = {'cartdata':window.cart};  
-    http.open("POST", url, true); 
-    http.setRequestHeader("Content-Type", "application/json");
+    MessengerExtensions.requestCloseBrowser();
+   // var url = 'http://www.studmenu.herokuapp.com/cart/'+JSON.stringify(window.cart);
+   // var params = {'cartdata':window.cart};  
+   // http.open("POST", url, true); 
+   // http.setRequestHeader("Content-Type", "application/json");
     
-    http.send(params);
+   // http.send(params);
 
   }
   plusbutton.onclick=function(arg,arg2,arg3){
