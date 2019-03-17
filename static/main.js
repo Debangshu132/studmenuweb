d89bc0b79cdaad6b5d6d92c3bec68b403ced65c0
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-03-17 10:50:58 +0530

    Update main.js

--- static/main.js
+++ static/main.js
@@ -209,16 +209,17 @@ document.getElementById("carticon").onclick=function(menuPicArray){
 //};
 MessengerExtensions.getUserID(function success(uids) {
   var psid = uids.psid;//This is your page scoped sender_id
-  
-}, function error(err) {
-  alert("Messenger Extension Error: " + err);
-});
-    var dataToSend={"id":psid,"cart":window.cart};
+  var dataToSend={"id":psid,"cart":window.cart};
     var http = new XMLHttpRequest();
     var url = 'https://studmenu.herokuapp.com/cart/'+JSON.stringify(dataToSend);
     http.open("POST", url, false);
     http.setRequestHeader("Content-Type", "application/json");
     http.send("abcd");
+  
+}, function error(err) {
+  alert("Messenger Extension Error: " + err);
+});
+    
     //window.open('http://studmenu.herokuapp.com/cart');
     MessengerExtensions.requestCloseBrowser((function success() {
        //webview closed
