616539f64332aee2074ff39561f5a4f86957f74f
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-03-17 22:54:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 #@app.route("/", methods=['GET', 'POST'])
 #def index():
 #    return "hello man"
+def getRestaurantsTableInformation(nameOfRestaurant,tableno):
+    MONGODB_URI = "mongodb://Debangshu:Starrynight.1@ds163694.mlab.com:63694/brilu"
+    client = MongoClient(MONGODB_URI, connectTimeoutMS=30000)
+    db = client.get_database("brilu")
+    col = db["restaurants"]
+    cursor = col.find()
+    restaurant = cursor[0]
+    return(restaurant[nameOfRestaurant]["tables"])
 @app.route("/", methods=['GET', 'POST'])
 def menu():
          MONGODB_URI="mongodb://Debangshu:Starrynight.1@ds163694.mlab.com:63694/brilu"
@@ -18,7 +26,11 @@ def menu():
          #return render_template('chart.html',menu=menu)
 @app.route("/individualcart/<data>", methods=['GET', 'POST'])
 def individualcart(data):
-         return render_template("individualcart.html",datatowrite = data)
+         restaurant=data["restaurant"]
+         tableno=data["tableno"]
+         consumer_id=data["consumer_id"]
+         mydata=getRestaurantsTableInformation(restaurant,tableno)
+         return render_template("individualcart.html",datatowrite = mydata)
 @app.route("/groupcart/<data>", methods=['GET', 'POST'])
 def groupcart(data):
          return data
