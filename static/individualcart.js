9cbf4f8627031efac44163c6d02a17fcf235e8f8
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-03-18 14:43:10 +0530

    Update individualcart.js

--- static/individualcart.js
+++ static/individualcart.js
@@ -6,7 +6,7 @@ window.onload = function() {
 
 function populateBody(){
    //document.getElementById("cartbody").innerHTML+= window.data;
-   var datajson=JSON.parse(window.data);
+   //var datajson=JSON.parse(window.data);
    //document.getElementById("cartbody").innerHTML+= window.data;
    //document.getElementById("cartbody").innerHTML+= window.data;
    //document.getElementById("cartbody").innerHTML+="1324";
