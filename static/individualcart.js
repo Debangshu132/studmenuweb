2ea14e767ba0826684911a504207da4888e88df9
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-03-18 16:30:27 +0530

    Update individualcart.js

--- static/individualcart.js
+++ static/individualcart.js
@@ -22,7 +22,7 @@ function populateBody(){
        var individualid=bucketlist[bucketitem];
        var cartitemlist=datajsoncart[individualid];
        var bucket=document.createElement('bucket');
-       document.getElementById("cartbody").innerHTML+= JSON.stringify(cartitemlist);
+       document.getElementById("cartbody").innerHTML+= JSON.stringify(JSON.parse(cartitemlist));
         
           for(var indexoforder=0;indexoforder<cartitemlist.length;indexoforder++)
           {
