6f8a77e29c0523aee67ba7f071ff8fc6b0854870
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-03-20 23:54:25 +0530

    Update individualcart.js

--- static/individualcart.js
+++ static/individualcart.js
@@ -1,7 +1,7 @@
 
 
 
-window.socket = io.connect('/' );
+window.socket = io.connect('http://studmenu.herokuapp.com' );
 window.socket.on('connect', function(msg) {
 console.log('connected');    
 });
