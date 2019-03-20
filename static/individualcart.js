e7bd487e6e4653148bc7b8c71dccd9f6d0a97877
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-03-20 18:15:17 +0530

    Update individualcart.js

--- static/individualcart.js
+++ static/individualcart.js
@@ -3,6 +3,7 @@ window.onload = function() {
    //window.cartitemjson=JSON.parse(window.data);
    populateBody();
    var socket = io.connect('http://' + document.domain+ ':' + location.port );
+   alert("yeay! now");
    socket.on('connect', function() {
       alert("yeay!");
       socket.emit('connected',{data:"testsend"});
