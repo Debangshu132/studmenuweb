a8db0094b93a4ca0c8d8bff4276ca9ab52270490
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-03-23 21:57:02 +0530

    Update main-js.js

--- static/assets/libs/js/main-js.js
+++ static/assets/libs/js/main-js.js
@@ -15,7 +15,7 @@ function populateBodyWaiter(datatogive){
        var card=document.createElement('div');
        card.className="card";
        card.innerHTML='<div class="card-header d-flex"><h4 class="mb-0" >Table number '+tableNo+'</h4><div class="dropdown ml-auto"><a class="toolbar" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="mdi mdi-dots-vertical"></i>  </a> <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuLink"><a class="dropdown-item" href="#">Checkout</a><a class="dropdown-item" href="#">Reserve</a><a class="dropdown-item" href="#" onclick="return logoutWaiter('+tableNo.toString()+');">Logout Waiter</a></div></div></div>';
-       card.innerHTML+='<iframe frameBorder="0" src="http://studmenuweb.herokuapp.com/groupcart/%7B%22restaurant%22:%22Taj%22,%22tableno%22:%22'+tableNo+'%22,%22identity%22:%22waiter%22%7D"></iframe>';
+       card.innerHTML+='<iframe frameBorder="0" src="http://studmenuweb.herokuapp.com/groupcart/%7B%22restaurant%22:%22Taj%22,%22tableno%22:%22'+tableNo+'%22,%22identity%22:%22manager%22%7D"></iframe>';
 
 
        tablesInner.appendChild(card);
