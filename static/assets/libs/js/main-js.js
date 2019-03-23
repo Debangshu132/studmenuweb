9ebe8286a9c3a2320ef476e8362d6ef0cfa7fea9
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-03-23 23:30:03 +0530

    Update main-js.js

--- static/assets/libs/js/main-js.js
+++ static/assets/libs/js/main-js.js
@@ -1,11 +1,9 @@
  window.onload = function() {
     var datatogive={"restaurant":"Taj","noOfTables":8};   
-    populateBodyWaiter(datatogive);
+    populateBodyTables(datatogive);
     }
-
-
-
-function populateBodyWaiter(datatogive){
+function populateBodyTables(datatogive){
+     clear("tableContainer");
      var noOfTables=datatogive["noOfTables"];
      for(var tableNo=0;tableNo<noOfTables;tableNo++){
         var tablesOuter=document.createElement('div');
@@ -16,18 +14,10 @@ function populateBodyWaiter(datatogive){
        card.className="card";
        card.innerHTML='<div class="card-header d-flex"><h4 class="mb-0" >Table number '+tableNo+'</h4><div class="dropdown ml-auto"><a class="toolbar" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="mdi mdi-dots-vertical"></i>  </a> <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuLink"><a class="dropdown-item" href="#">Checkout</a><a class="dropdown-item" href="#">Reserve</a><a class="dropdown-item" href="#" onclick="return logoutWaiter('+tableNo.toString()+');">Logout Waiter</a></div></div></div>';
        card.innerHTML+='<iframe frameBorder="0" src="http://studmenuweb.herokuapp.com/groupcart/%7B%22restaurant%22:%22Taj%22,%22tableno%22:%22'+tableNo+'%22,%22identity%22:%22manager%22%7D"></iframe>';
-
-
        tablesInner.appendChild(card);
        tablesOuter.appendChild(card);
-       document.getElementById('tableContainer').appendChild(tablesOuter);
-
-
-     }
- } 
+       document.getElementById('tableContainer').appendChild(tablesOuter);}} 
  
- 
-
  function logoutWaiter(tableno){
     var check=confirm('do you really want to logout the waiter?')
     if(check===true){
@@ -36,7 +26,14 @@ function populateBodyWaiter(datatogive){
         http.open("POST", url, false); 
         http.setRequestHeader("Content-Type", "application/json");
         http.send();
-        alert('logged out')
+        alert('logged out')}}
 
-    }
- }
+function clear(element){document.getElementById(element).innerHTML="";}       
+
+function populateBodyMenu(){
+    clear("wrapper");
+    var menuwrapper=document.createElement('div');
+    menuwrapper.className="menuwrapper";
+    menuwrapper.innerHTML='<iframe frameBorder="0" src="http://studmenuweb.herokuapp.com/Taj"></iframe>';
+    document.getElementById('wrapper').appendChild(menuwrapper);   
+}
