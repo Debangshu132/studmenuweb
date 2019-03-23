08d9c92c000740d05b47275c29b777306e5fefd4
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-03-23 21:40:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -88,10 +88,10 @@ def dashboard(restoo):
 def dashboardwaiterpage():
      
      return render_template("dashboard.html")
-@app.route("/logoutwaiter/<tablejson>", methods=['GET', 'POST'])
-def logoutwaiter(tablejson):
-     restaurant = request.cookies.get('restaurantcookie')
-     table=json.loads(tablejson)
+@app.route("/logoutwaiter/<tablejsondata>", methods=['GET', 'POST'])
+def logoutwaiter(tablejsondata):
+     restaurant = "Taj"
+     table=json.loads(tablejsondata)
      tableno=table['tableno']
      updateRestaurantsTablesInformation(restaurant,str(tableno), waiter={})   
      return (0)
