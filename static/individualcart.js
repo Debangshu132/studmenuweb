17bdcf38f1e349e25464e51ef46c576f3f6b5a0b
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-03-23 14:05:10 +0530

    Update individualcart.js

--- static/individualcart.js
+++ static/individualcart.js
@@ -277,13 +277,13 @@ function populateBody(datatogive){
          
       if(noOfPendingOrders>0){
          var redness=200+noOfPendingOrders*10;
-         var greenness=100-noOfPendingOrders*10;
-         var blueness=100-noOfPendingOrders*10;
+         var greenness=200-noOfPendingOrders*10;
+         var blueness=200-noOfPendingOrders*10;
          if((redness<255) && (blueness>0) && (greenness)>0){
 
          document.getElementById("cartbody").style.borderTop="3px solid rgb("+redness.toString(10)+","+greenness.toString(10)+","+blueness.toString(10)+")";}
        else{
-          alert("nope");
+         
          document.getElementById("cartbody").style.borderTop="3px solid rgb(255,0,0)";
        } 
       }
