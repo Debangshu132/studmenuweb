d86ce8cda1e36a11bfcd6eabc3df7def0fdefabf
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-03-24 11:20:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def populatedashboardmenuitems(resto):
          cursor = col.find()
          restaurant = cursor[0]
          print(restaurant[resto]["menu"])
-         return JSON.dumps(menu) 
+         return json.dumps(menu) 
 
 
 def updateRestaurantsTablesInformation(nameOfRestaurant,tableno, **kwargs):
