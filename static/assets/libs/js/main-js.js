b8e7119bd01e3e7074974fdab5c68c44d16e057d
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-03-28 15:41:33 +0530

    Update main-js.js

--- static/assets/libs/js/main-js.js
+++ static/assets/libs/js/main-js.js
@@ -149,7 +149,9 @@ function populateBodyWaiter(){
            
             for(var waiteridindex=0;waiteridindex<waiteridarray.length;waiteridindex++){
              var waiterid=waiteridarray[waiteridindex];   
-            htmlstring=htmlstring+'<tr><td><div class="m-r-10"><img src="assets/images/github.png" alt="user" width="35"></div></td><td>name </td><td>Rs. 100</td><td>'+waiterid+'</td><td><div class="switch-button switch-button-success"><input type="checkbox"  name="switch12" onclick="updateCheckboxesMenu(this)" checked value=1 id=2><span><label for=2></label></span></div></td><td><div class="input-group-append be-addon"><button type="button" data-toggle="dropdown" class="btn btn-outline-secondary dropdown-toggle">Edit</button><div class="dropdown-menu"><a href="#" class="dropdown-item">Edit</a><a href="#" class="dropdown-item">Another action</a><a href="#" class="dropdown-item">Something else here</a><div class="dropdown-divider"></div><a href="#" class="dropdown-item">Settings</a></div></div></div></td></tr>';
+             var waitername=datajson[waiterid]['name'];
+             var waiteractivetables=datajson[waiterid]['activetables'];
+            htmlstring=htmlstring+'<tr><td><div class="m-r-10"><img src="assets/images/github.png" alt="user" width="35"></div></td><td>'+JSON.stringify(waitername)+' </td><td>'+waiterid+'</td><td>'+JSON.stringify(waiteractivetables)+'</td><td><div class="switch-button switch-button-success"><input type="checkbox"  name="switch12" onclick="updateCheckboxesMenu(this)" checked value=1 id=2><span><label for=2></label></span></div></td><td><div class="input-group-append be-addon"><button type="button" data-toggle="dropdown" class="btn btn-outline-secondary dropdown-toggle">Edit</button><div class="dropdown-menu"><a href="#" class="dropdown-item">Edit</a><a href="#" class="dropdown-item">Another action</a><a href="#" class="dropdown-item">Something else here</a><div class="dropdown-divider"></div><a href="#" class="dropdown-item">Settings</a></div></div></div></td></tr>';
             }
 
             formdiv.innerHTML=htmlstring+'</tbody></table></div></div></div>';
