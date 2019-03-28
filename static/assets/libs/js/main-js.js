9a9370745d3e6cfc46adf901352eff0f870c13c4
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-03-28 16:26:47 +0530

    Update main-js.js

--- static/assets/libs/js/main-js.js
+++ static/assets/libs/js/main-js.js
@@ -154,10 +154,9 @@ function populateBodyWaiter(){
              waitername=JSON.stringify(waitername);
              waitername = waitername.substring(1, waitername.length-1);
              var waiteractivetables=datajson[waiterid]['activetables'];
-             waiteractivetables=JSON.stringify(waiteractivetables);
-             waiteractivetables = waiteractivetables.substring(1, waiteractivetables.length-1);
-            htmlstring=htmlstring+'<tr><td><div class="m-r-10"><img src="assets/images/github.png" alt="user" width="35"></div></td><td>'+waitername+' </td><td>'+waiterid+'</td><td>'+waiteractivetables+'</td><td><div class="input-group-append be-addon"><button type="button" data-toggle="dropdown" class="btn btn-outline-secondary dropdown-toggle">Edit</button><div class="dropdown-menu"><a href="#" class="dropdown-item">Edit</a><a href="#" class="dropdown-item">Another action</a><a href="#" class="dropdown-item">Something else here</a><div class="dropdown-divider"></div><a href="#" class="dropdown-item">Settings</a></div></div></div></td></tr>';
-            }
+            for(var tableindex=0;tableindex<waiteractivetables.length;tableindex++){
+            htmlstring=htmlstring+'<tr><td><div class="m-r-10"><img src="assets/images/github.png" alt="user" width="35"></div></td><td>'+waitername+' </td><td>'+waiterid+'</td><td>'+'<td><div class="pagination pagination-sm"><li class="page-item"><a class="page-link" href="#">'+JSON.stringify(tableindex)+'</a></li></div></td>'+'</td><td><div class="input-group-append be-addon"><button type="button" data-toggle="dropdown" class="btn btn-outline-secondary dropdown-toggle">Edit</button><div class="dropdown-menu"><a href="#" class="dropdown-item">Edit</a><a href="#" class="dropdown-item">Another action</a><a href="#" class="dropdown-item">Something else here</a><div class="dropdown-divider"></div><a href="#" class="dropdown-item">Settings</a></div></div></div></td></tr>';
+            }}
 
             formdiv.innerHTML=htmlstring+'</tbody></table></div></div></div>';
                 document.getElementById('wrapper').appendChild(formdiv);
