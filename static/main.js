7d01f47aecd8432232332ad88b8b4ab6aad2f256
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-04-04 12:10:25 +0530

    Update main.js

--- static/main.js
+++ static/main.js
@@ -172,20 +172,20 @@ document.getElementById("cartcontainer").onclick=function(menuPicArray){
   cartitempricename.innerHTML="&#x20b9; "+JSON.stringify(window.cart[i]["price"]);
   cartitemquantityname.innerHTML=JSON.stringify(window.cart[i]["quantity"]);
   cartorderbutton.innerHTML="ORDER";
-  window.ordergif=document.createElement('IMG');
+  
+  cartorderbutton.onclick=function(){
+    clearFooter("cart");
+    window.ordergif=document.createElement('IMG');
   window.ordergif.className='ordergif';
   window.ordergif.style.width="100%";
-  window.ordergif.style.height="0%";
+
   window.ordergif.src="https://storage.googleapis.com/meallionpics/General/Icons/Order.gif";
   
   cart.appendChild(window.ordergif);
-  cartorderbutton.onclick=function(){
-    clearFooter("cart");
-    cart.innerHTML='<img src="https://storage.googleapis.com/meallionpics/General/Icons/Order.gif" >';
-    //window.ordergif.style.height="40%";
-    //cartorderbutton.style.background="#2adeb1";
+    window.ordergif.style.height="40%";
+    cartorderbutton.style.background="#2adeb1";
     cart.style.background="#2adeb1";
-    cartorderbutton.innerHTML="Processing....."
+    //cartorderbutton.innerHTML="Processing..."
     //cart.innerHTML="<img src='https://storage.googleapis.com/meallionpics/General/Icons/Order.gif'>";
   MessengerExtensions.getUserID(function success(uids) {
     
