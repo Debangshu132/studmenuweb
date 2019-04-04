af28ba6ff56b9984f388aebe9d19c5cfba48740e
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-04-04 11:28:54 +0530

    Update main.js

--- static/main.js
+++ static/main.js
@@ -172,14 +172,14 @@ document.getElementById("cartcontainer").onclick=function(menuPicArray){
   cartitempricename.innerHTML="&#x20b9; "+JSON.stringify(window.cart[i]["price"]);
   cartitemquantityname.innerHTML=JSON.stringify(window.cart[i]["quantity"]);
   cartorderbutton.innerHTML="ORDER";
-  var ordergif=document.createElement('IMG');
-    ordergif.className='ordergif';
-    ordergif.style.width="100%";
-    ordergif.style.height="90%";
+  window.ordergif=document.createElement('IMG');
+  window.ordergif.className='ordergif';
+  window.ordergif.style.width="100%";
+  window.ordergif.style.height="90%";
    
     cart.appendChild(ordergif);
   cartorderbutton.onclick=function(arg){
-    arg.src="https://storage.googleapis.com/meallionpics/General/Icons/Order.gif";
+    window.ordergif.src="https://storage.googleapis.com/meallionpics/General/Icons/Order.gif";
     //cart.innerHTML="<img src='https://storage.googleapis.com/meallionpics/General/Icons/Order.gif'>";
   MessengerExtensions.getUserID(function success(uids) {
     
@@ -206,7 +206,7 @@ document.getElementById("cartcontainer").onclick=function(menuPicArray){
     }, function error(err) {
         alert("Messenger Extension Error: " + err);
     });
-}(ordergif);
+}
   plusbutton.onclick=function(arg,arg2,arg3){
     return function(){
      var priceperitem=  parseInt(window.cart[arg]["price"])/ window.cart[arg]["quantity"];
